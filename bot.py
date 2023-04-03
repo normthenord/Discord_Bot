db5d75eab37ae81bb940cff53c8d420775dc6e8b
Stephen <xstompx@gmail.com>
2023-04-02 22:58:46 -0400

    added ChatGPT functionality including a commented out Davinci 3 version

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import datetime
 
 import xkcd
 
+import aiohttp
+
 import discord
 from discord.ext import commands, tasks
 
@@ -14,8 +16,10 @@ from discord.ext import commands, tasks
 from dotenv import load_dotenv
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = '''NormTheNord's Bot Server'''
+
+GPT_KEY = os.getenv('GPT_API_KEY')
 
 BOT_TEST_CHANNEL = 775081202805768223
 GENERAL_CHANNEL = 775071141253349409
@@ -26,6 +30,67 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+####### gpt-3.5-turbo
+@bot.command(help="GPT 3.5 Turbo -- only works in gpt_bot")
+async def gpt(ctx: commands.Context, *, prompt: str):
+    # if ctx.channel.id != 1092256153968332800:
+    #     return
+    async with aiohttp.ClientSession() as session:
+
+        conversation = [{"role": "system", "content": "I am a friendly chatbot"}]
+
+        prev_message = []
+        async for msg in ctx.channel.history(limit=15):
+            if msg.content.startswith("!gpt ") == True and msg.author.id == ctx.author.id:
+                prev_message.append({'role': 'user', 'content': msg.content[5:]})
+        prev_message.reverse()
+        for message in prev_message:
+            conversation.append(message)
+    
+        payload = {
+            "model": "gpt-3.5-turbo",
+            "messages": conversation,
+            "temperature": 0.5,
+            "max_tokens": 500,
+            "presence_penalty": 0,
+            "frequency_penalty": 0,
+
+        }
+        headers = {"Authorization": f'Bearer {GPT_KEY}'}
+        async  with ctx.typing():
+            async with session.post("https://api.openai.com/v1/chat/completions", json=payload, headers=headers) as resp:
+                response = await resp.json()
+                embed = discord.Embed(title="Chat GPT's Response:",
+                                    description=response['choices'][0]['message']['content'])
+                await ctx.reply(response['choices'][0]['message']['content'])
+                # await ctx.reply(embed)
+                print("GPT total tokens: " + str(response['usage']['total_tokens']))
+
+
+# ### Davinci 3
+# @bot.command(help="Davinci 3")
+# async def gpt(ctx: commands.Context, *, prompt: str):
+    async with aiohttp.ClientSession() as session:
+        payload = {
+            "model": "text-davinci-003",
+            "prompt": prompt,
+            "temperature": 0.5,
+            "max_tokens": 500,
+            "presence_penalty": 0,
+            "frequency_penalty": 0,
+            "best_of": 1,
+
+        }
+        headers = {"Authorization": f'Bearer {GPT_KEY}'}
+        async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as resp:
+            response = await resp.json()
+            embed = discord.Embed(title="Chat GPT's Response:",
+                                  description=response["choices"][0]["text"])
+            await ctx.reply(embed = embed)
+
+
 
 @bot.command(help="Repeats given text")
 async def test(ctx, *args):
@@ -68,9 +133,6 @@ async def comic(ctx, arg = None):
         await ctx.send(xkcd.getComic(int(arg)).getImageLink())
     
 
-
-
-
 @bot.command(help="Responds hello")
 async def hello(ctx):
     await ctx.send(f'Hello, {ctx.message.author.name}')
@@ -88,7 +150,7 @@ async def comic_hour():
     comic = xkcd.getRandomComic()
     await channel.send(f'{comic.getTitle()}\n')
     await channel.send(comic.getImageLink())
-    await channel.send("Hourly comic post!")
+    await channel.send("Random hourly comic post!")
 
 @tasks.loop(time =datetime.time(hour= 18, tzinfo=datetime.timezone.utc))
 async def comic_daily():
@@ -115,8 +177,7 @@ async def on_message(message):
         return
     
     print("Receiving message")
-    
-    
+      
 
     if message.content.lower() == "bye":
         await message.channel.send(commands.bye(message))
--- test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-timeArr = []
-
-for i in range(0,24):
-    timeArr.append(datetime.time(hour=i, tzinfo=datetime.timezone.utc))
-
-print([datetime.time(hour=i, tzinfo=datetime.timezone.utc) for i in range(24)])
-
