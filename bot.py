c14df4ce202d7ecbacc7f1dc862bf3c393c7e684
Stephen <xstompx@gmail.com>
2023-04-03 22:52:26 -0400

    Changed to a cogs system for all commands

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import balls
 
+import asyncio
 
 import random
 import os
@@ -21,16 +22,10 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = '''NormTheNord's Bot Server'''
 
-GPT_KEY = os.getenv('GPT_API_KEY')
-
 BOT_TEST_CHANNEL = 775081202805768223
 GENERAL_CHANNEL = 775071141253349409
 
 EVERY_HOUR = [datetime.time(hour=i, tzinfo=datetime.timezone.utc) for i in range(24)]
-
-
-openai.api_key = GPT_KEY
-
 
 
 
@@ -39,136 +34,11 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py") and filename != "__init__.py":
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
-@bot.command(help="Get AI Generated image (1024*1024)")
-async def GPTImage(ctx:commands.Context, *, prompt: str):
-    
-    async with aiohttp.ClientSession() as session:
-        
-        async  with ctx.typing():
-            response = openai.Image.create(
-            prompt=prompt,
-            n=1,
-            size="1024x1024"
-            )
-            image_url = response['data'][0]['url']
-
-            await ctx.reply(image_url)
-
-
-
-##############Bot commands###################
-
-
-@bot.command(help="GPT 3.5 Turbo -- only works in gpt_bot")
-async def GPT(ctx: commands.Context, *, prompt: str):
-    # if ctx.channel.id != 1092256153968332800:
-    #     return
-    async with aiohttp.ClientSession() as session:
-
-        conversation = [{"role": "system", "content": "I am a friendly chatbot"}]
-
-        prev_message = []
-        async for msg in ctx.channel.history(limit=15):
-            if msg.content.startswith("!gpt ") == True and msg.author.id == ctx.author.id:
-                prev_message.append({'role': 'user', 'content': msg.content[5:]})
-        prev_message.reverse()
-        for message in prev_message:
-            conversation.append(message)
-    
-        payload = {
-            "model": "gpt-3.5-turbo",
-            "messages": conversation,
-            "temperature": 0.5,
-            "max_tokens": 500,
-            "presence_penalty": 0,
-            "frequency_penalty": 0,
-
-        }
-        headers = {"Authorization": f'Bearer {GPT_KEY}'}
-        async  with ctx.typing():
-            async with session.post("https://api.openai.com/v1/chat/completions", json=payload, headers=headers) as resp:
-                response = await resp.json()
-                # embed = discord.Embed(title="Chat GPT's Response:",
-                #                     description=response['choices'][0]['message']['content'])
-                await ctx.reply(response['choices'][0]['message']['content'])
-                print("GPT total tokens: " + str(response['usage']['total_tokens']))
-
-
-# ### Davinci 3
-@bot.command(help="Davinci 3")
-async def Davinci(ctx: commands.Context, *, prompt: str):
-    async with aiohttp.ClientSession() as session:
-        payload = {
-            "model": "text-davinci-003",
-            "prompt": prompt,
-            "temperature": 0.5,
-            "max_tokens": 500,
-            "presence_penalty": 0,
-            "frequency_penalty": 0,
-            "best_of": 1,
-
-        }
-        headers = {"Authorization": f'Bearer {GPT_KEY}'}
-        async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as resp:
-            async  with ctx.typing():
-                response = await resp.json()
-                embed = discord.Embed(title="Chat GPT's Response:",
-                                    description=response["choices"][0]["text"])
-                await ctx.reply(embed = embed)
-
-
-
-@bot.command(help="Repeats given text")
-async def test(ctx, *args):
-    arguments = ' '.join(args)
-    await ctx.reply(arguments)
-
-@bot.command(help="Returns a random number between 1 and number given (default 10)")
-async def roll(ctx, arg=10):
-    await ctx.reply(f'Rolling between 1 and {arg}: {random.randint(1,int(arg))}')
-
-@bot.command(help="Pongs")
-async def ping(ctx):
-    await ctx.reply("pong")
-
-@bot.command(name="xkcd",
-             help="Posts a random xkcd comic. Can also provide a number for specific comic")
-async def comic(ctx, arg = None):
-    if arg == None:
-        comic = xkcd.getRandomComic()
-        await ctx.send(f'{comic.getTitle()}\n')
-        await ctx.send(comic.getImageLink())
-    elif int(arg) >= xkcd.getLatestComicNum():
-        await ctx.send(f'{xkcd.getLatestComic().getTitle()}\n')
-        await ctx.send(xkcd.getLatestComic().getImageLink())
-    elif int(arg) > 0 and int(arg) <= xkcd.getLatestComicNum():
-        await ctx.send(f'{xkcd.getComic(int(arg)).getTitle()}\n')
-        await ctx.send(xkcd.getComic(int(arg)).getImageLink())
-
-@bot.command(help="Posts a random xkcd comic. Can also provide a number for specific comic")
-async def comic(ctx, arg = None):
-    if arg == None:
-        comic = xkcd.getRandomComic()
-        await ctx.send(f'{comic.getTitle()}\n')
-        await ctx.send(comic.getImageLink())
-    elif int(arg) >= xkcd.getLatestComicNum():
-        await ctx.send(f'{xkcd.getLatestComic().getTitle()}\n')
-        await ctx.send(xkcd.getLatestComic().getImageLink())
-    elif int(arg) > 0 and int(arg) <= xkcd.getLatestComicNum():
-        await ctx.send(f'{xkcd.getComic(int(arg)).getTitle()}\n')
-        await ctx.send(xkcd.getComic(int(arg)).getImageLink())
-    
-
-@bot.command(help="Responds hello")
-async def hello(ctx):
-    await ctx.send(f'Hello, {ctx.message.author.name}')
-
-
-@bot.command(name="8ball",
-             help="Ask the magic 8 ball a question")
-async def ball(ctx):
-    await ctx.reply(balls.ball_answers[random.randint(1,int(len(balls.ball_answers)))])
 
 
 #############Looping Tasks#################
@@ -223,6 +93,9 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(TOKEN)
 
-
-bot.run(TOKEN)
+asyncio.run(main())
---
+++ cogs/openai.py
@@ -0,0 +1,104 @@
+from discord.ext import commands
+
+import openai
+import aiohttp
+
+import os
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+GPT_KEY = os.getenv('GPT_API_KEY')
+openai.api_key = GPT_KEY
+
+
+class OpenAI(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(help="GPT 3.5 Turbo")
+    async def GPT(self, ctx: commands.Context, *, prompt: str):
+
+        if ctx.channel.id != 1092256153968332800:
+            ctx.reply("Must be in gpt_bot")
+            return
+        async with aiohttp.ClientSession() as session:
+
+            self.conversation = [{"role": "system", "content": "I am a friendly chatbot"}]           
+            
+            self.prev_message = []
+            async for msg in ctx.channel.history(limit=15):
+                if msg.content.startswith("!GPT ") == True and msg.author.id == ctx.author.id:
+                    self.prev_message.append({'role': 'user', 'content': msg.content[5:]})
+            self.prev_message.reverse()
+            for message in self.prev_message:
+                self.conversation.append(message)
+        
+
+            self.payload = {
+                "model": "gpt-3.5-turbo",
+                "messages": self.conversation,
+                "temperature": 0.5,
+                "max_tokens": 500,
+                "presence_penalty": 0,
+                "frequency_penalty": 0,
+            }
+
+
+
+            self.headers = {"Authorization": f'Bearer {GPT_KEY}'}
+
+            async  with ctx.typing():
+                async with session.post("https://api.openai.com/v1/chat/completions", json=self.payload, headers=self.headers) as resp:
+                    self.response = await resp.json()
+                    print(self.response)
+                    # embed = discord.Embed(title="Chat GPT's Response:",
+                    #                     description=response['choices'][0]['message']['content'])
+                    await ctx.reply(self.response['choices'][0]['message']['content'])
+                    print("GPT total tokens: " + str(self.response['usage']['total_tokens']))
+
+
+    @commands.command(help="Get AI Generated image (1024*1024)")
+    async def GPTImage(self, ctx:commands.Context, *, prompt: str):
+        
+        async with aiohttp.ClientSession() as session:
+            
+            async  with ctx.typing():
+                response = openai.Image.create(
+                prompt=prompt,
+                n=1,
+                size="1024x1024"
+                )
+                image_url = response['data'][0]['url']
+
+                await ctx.reply(image_url)
+
+
+
+
+    @commands.command(help="Davinci 3")
+    async def Davinci(self, ctx: commands.Context, *, prompt: str):
+        async with aiohttp.ClientSession() as session:
+            self.payload = {
+                "model": "text-davinci-003",
+                "prompt": prompt,
+                "temperature": 0.5,
+                "max_tokens": 500,
+                "presence_penalty": 0,
+                "frequency_penalty": 0,
+                "best_of": 1,
+
+            }
+            self.headers = {"Authorization": f'Bearer {GPT_KEY}'}
+            async with session.post("https://api.openai.com/v1/completions", json=self.payload, headers=self.headers) as resp:
+                async with ctx.typing():
+                    self.response = await resp.json()
+                    # embed = discord.Embed(title="Chat GPT's Response:",
+                    #                     description=response["choices"][0]["text"])
+                await ctx.reply(self.response["choices"][0]["text"])
+
+
+async def setup(bot):
+    await bot.add_cog(OpenAI(bot))
