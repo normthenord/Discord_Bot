b230c9e37288e1bf07574f4e96e46036b52462a1
Stephen <xstompx@gmail.com>
2023-04-09 12:49:57 -0400

    added cat image. It was annoying

--- cogs/animals.py
+++ cogs/animals.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord
 
 import aiohttp
+import requests
 
 class Animals(commands.Cog):
     
@@ -20,7 +21,21 @@ class Animals(commands.Cog):
 
                 await ctx.send(embed=embed)
 
-
+    @commands.command()
+    async def cat(self, ctx: commands.Context, *, args=""):
+        url = "https://cataas.com/cat?json=true"
+        if args != "":
+            prompt = "".join(args)
+            prompt = prompt.replace(" ", "%20")
+            url = f'https://cataas.com/cat/says/{prompt}?json=true'
+        resp = requests.get(url)
+        data = resp.json()
+        embed = discord.Embed(title = "Meow")
+        embed.set_image(url=f"https://cataas.com{data['url']}")
+        embed.set_footer(text="Cataas")
+        await ctx.reply(embed=embed)
+                    
+                    
 
 async def setup(bot):
     await bot.add_cog(Animals(bot))
