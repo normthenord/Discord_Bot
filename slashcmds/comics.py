6646f247a107fc20b018d8bf85255c4ab24fd09e
Stephen <xstompx@gmail.com>
2023-04-10 17:28:14 -0400

    Changed comic_daily so that it doesn't repost as often

--- slashcmds/comics.py
+++ slashcmds/comics.py
@@ -14,21 +14,18 @@ class Comics(app_commands.Group):
             embed = discord.embeds.Embed(title=comic.getTitle())
             embed.set_image(url=comic.getImageLink())
             await interaction.followup.send(embed=embed)
-            # await interaction.followup.send(f'{comic.getTitle()}\n')
-            # await interaction.followup.send(comic.getImageLink())
+
         elif int(comic_num) >= xkcd_module.getLatestComicNum():
             embed = discord.embeds.Embed(title=xkcd_module.getLatestComic().getTitle())
             embed.set_image(url=xkcd_module.getLatestComic().getImageLink())
             embed.set_footer(text="There aren't that many comics yet!")
             await interaction.followup.send(embed=embed)
-            # await interaction.followup.send(f'{xkcd_module.getLatestComic().getTitle()}\n')
-            # await interaction.followup.send(xkcd_module.getLatestComic().getImageLink())
+
         elif int(comic_num) > 0 and int(comic_num) <= xkcd_module.getLatestComicNum():
             embed = discord.embeds.Embed(title=xkcd_module.getComic(int(comic_num)).getTitle())
             embed.set_image(url=xkcd_module.getComic(int(comic_num)).getImageLink())
             await interaction.followup.send(embed=embed)
-            # await interaction.followup.send(f'{xkcd_module.getComic(int(comic_num)).getTitle()}\n')
-            # await interaction.followup.send(xkcd_module.getComic(int(comic_num)).getImageLink())
+
 
 
 async def setup(bot):
