9cc057c8ac991af98d084649b94a5b289b9299d4
Stephen Burwell <xstompx@gmail.com>
2025-05-11 14:02:30 -0400

    Hopefully making error handling work

--- slashcmds/ai.py
+++ slashcmds/ai.py
@@ -19,7 +19,7 @@ class AI(app_commands.Group):
                 "prompt": f"{prompt}. Try to make your response 2000 characters or less",
                 "stream": False  # Set to True if you want streaming output
             }
-
+            
             try:
                 response = requests.post(url, json=payload, headers=headers)
                 response.raise_for_status()
@@ -31,48 +31,8 @@ class AI(app_commands.Group):
             except requests.RequestException as e:
                 await interaction.followup.send(f"Error: Server probably asleep")
 
-    # @app_commands.command(description="Get AI Generated image")
-    # async def image(self, interaction: discord.Interaction, img_prompt: str):
-    #     await interaction.response.defer()
+   
 
-    #     async with aiohttp.ClientSession() as session:
-    #         url = "https://api.limewire.com/api/image/generation"
-    #         header = {"Content-Type": "application/json",
-    #                   "X-Api-Version": "v1",
-    #                   "Accept": "application/json",
-    #                   "Authorization": f"Bearer {LIMEWIRE_KEY}"}
-
-    #         payload = payload = {"prompt": img_prompt,
-    #                              "aspect_ratio": "1:1"}
-
-    #         r = requests.post(url=url, json=payload, headers=header)
-    #         if r.status_code == 200:
-    #             data = r.json()
-
-    #             print(f"Credits remaining: {data['credits_remaining']}")
-    #             await interaction.followup.send(data['data'][0]['asset_url'])
-    #         else:
-    #             await interaction.followup.send("Out of credits")
-
-    # @app_commands.command(description="mixtral-8x7b-32768")
-    # async def chat(self, interaction: discord.Interaction, prompt: str):
-    #     await interaction.response.defer()
-    #     async with aiohttp.ClientSession() as session:
-    #         try:
-    #             client = groq.Client(api_key=settings.GROQ_KEY)
-    #             self.result = client.chat.completions.create(
-    #                 messages=[{
-    #                     "role": "user",
-    #                     "content": prompt,
-    #                 }
-    #                 ],
-    #                 model="mixtral-8x7b-32768",
-    #             )
-    #             print(f"Total tokens used: {self.result.usage.total_tokens}")
-    #             await interaction.followup.send(self.result.choices[0].message.content)
-    #         except Exception as e:
-    #             print(e)
-
-
+    
 async def setup(bot):
     bot.tree.add_command(AI(name="ai", description="AI commands"))
